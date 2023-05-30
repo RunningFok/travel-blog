50d2987692743d468c22a3e00e3250ea16b7e4b0
runningfok <adrianfok26@gmail.com>
2023-05-30 16:54:13 +0800

    updated nextconfig setting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env:{
+        DATABASE_URL: process.env.DATABASE_URL,
+        GOOGLE_API_KEY: process.env.GOOGLE_API_KEY
+    }
+}
 
 module.exports = nextConfig
