08e29d11c73c625d16cb58524afb3018772bfe49
runningfok <adrianfok26@gmail.com>
2023-05-31 00:24:37 +0800

    changed config of prcoess.env from prisma client

--- src/app/api/client.ts
+++ src/app/api/client.ts
@@ -1,14 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-import * as dotenv from 'dotenv'
-
-dotenv.config()
-console.log(process.env.DATABASE_URL)
-
 
 let prismaInit: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
-  prismaInit = new PrismaClient();
+  prismaInit = new PrismaClient({
+    datasources:{
+      db:{
+        url: process.env.DATABASE_URL
+      }
+    }
+  });
 } else {
   if (!(global as any).prisma) {
     (global as any).prisma = new PrismaClient();
