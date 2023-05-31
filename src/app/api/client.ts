30fbfd80e9e824c232f5a86ba4276522f0338cf0
runningfok <adrianfok26@gmail.com>
2023-05-31 11:23:41 +0800

    changed prismaclient datasources for databseurl

--- src/app/api/client.ts
+++ src/app/api/client.ts
@@ -3,7 +3,13 @@ import { PrismaClient } from "@prisma/client";
 let prismaInit: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
-  prismaInit = new PrismaClient();
+  prismaInit = new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 } else {
   if (!(global as any).prisma) {
     (global as any).prisma = new PrismaClient();
