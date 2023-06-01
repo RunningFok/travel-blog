75740bb65925c924012729b961f2dcfaa9e7753a
runningfok <adrianfok26@gmail.com>
2023-06-01 11:42:23 +0800

    removed pl-5 on sub box

--- src/app/shared/Subscription.tsx
+++ src/app/shared/Subscription.tsx
@@ -3,14 +3,14 @@ type Props = {};
 export default function Subsecription(props: Props) {
   return (
     <div className="bg-[#F4F4F4] border flex flex-col sm:flex-col md:flex-row lg:flex-row md:justify-between lg:justify-between items-start px-20 py-5">
-      <div className="px-5 flex flex-col">
+      <div className="md:px-5 flex flex-col">
         <h5 className="font-bold text-lg text-wh-900 ">Get us in your inbox</h5>
         <h6 className="text-wh-900 text-md w-6/8">
           Sign up to our newsletter for the latest and greatest from your city
           and beyond
         </h6>
       </div>
-      <div className="flex flex-col pl-5 pt-5 md:pl-10 md:pr-5 lg:pl-10 lg:pr-5">
+      <div className="flex flex-col pt-5 md:pl-10 md:pr-5 lg:pl-10 lg:pr-5">
         <input
           className="text-left w-11/12 min-w-[100px] px-5 py-2 border rounded-sm"
           placeholder="Enter email address"
